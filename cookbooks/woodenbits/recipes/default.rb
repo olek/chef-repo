bccf759813129b3f3fa07b338ef9cdd1293f1800
Olek Poplavsky <olek@woodenbits.com>
2012-06-14 09:34:28 -0400

    added core RC files setting up base bash/vim environment

--- cookbooks/woodenbits/recipes/default.rb
+++ cookbooks/woodenbits/recipes/default.rb
@@ -12,8 +12,8 @@ end
 template "/etc/apt/sources.list" do
   source "sources.list.#{node[:platform_version]}.erb"
   mode 0644
-  owner "root"
-  group "root"
+  owner user
+  group user
   notifies :run, resources(:execute => "update system"), :immediately
   not_if { File.exists?("/etc/apt/sources.list.no_chef") }
 end
@@ -21,32 +21,10 @@ end
 template "/etc/crontab" do
   source "crontab.erb"
   mode 0644
-  owner "root"
-  group "root"
-end
-
-directory "/root/build" do
-  owner "root"
-  group "root"
-  mode "0755"
-  action :create
+  owner user
+  group user
 end
 =end
-
-directory "/root/tmp" do
-  owner "root"
-  group "root"
-  mode "0700"
-  action :create
-end
-
-directory "/root/tmp/vi" do
-  owner "root"
-  group "root"
-  mode "0700"
-  action :create
-end
-
 
 if node[:platform] == 'ubuntu' && node[:platform_version] == '12.04'
   template "/etc/polkit-1/localauthority/50-local.d/com.ubuntu.enable-hibernate.pkla" do
@@ -80,48 +58,140 @@ template "/etc/bash.bashrc" do
   group "root"
 end
 
-template "/root/.profile" do
+template "#{home}/.profile" do
   source "profile.erb"
   mode 0644
-  owner "root"
-  group "root"
+  owner user
+  group user
 end
 
-template "/root/.bashrc" do
+template "#{home}/.bashrc" do
   source "root-bashrc.erb"
   mode 0644
-  owner "root"
-  group "root"
-end
-
-template "/root/.vimrc" do
-  source "vimrc.erb"
-  mode 0644
-  owner "root"
-  group "root"
-end
-
-template "/root/.inputrc" do
-  source "inputrc.erb"
-  mode 0644
-  owner "root"
-  group "root"
-end
-
-template "/root/.screenrc" do
-  source "screenrc.erb"
-  variables(
-    :is_root => true,
-    :logs_home => '/var/log'
-  )
-  mode 0640
-  owner "root"
-  group "root"
-  not_if { node[:penthera][:services].include?('workstation') }
+  owner user
+  group user
 end
 =end
 
+%w(root olek).each do |user|
+  home = user == 'root' ? "/#{user}" : "/home/#{user}"
+
+  directory "#{home}/tmp" do
+    owner user
+    group user
+    mode "0700"
+    action :create
+  end
+
+  directory "#{home}/tmp/vi" do
+    owner user
+    group user
+    mode "0700"
+    action :create
+  end
+
+  directory "#{home}/build" do
+    owner user
+    group user
+    mode "0750"
+    action :create
+  end
+
+  directory "#{home}/.bashrc.d" do
+    owner user
+    group user
+    mode "0750"
+    action :create
+  end
+
+  template "#{home}/.gitconfig" do
+    source "gitconfig.erb"
+    variables(
+      :username => 'olek',
+      :name => 'Olek Poplavsky',
+      :email => 'olek@woodenbits.com'
+    )
+    mode 0640
+    owner user
+    group user
+  end
+
+  template "#{home}/.screenrc" do
+    source "screenrc.erb"
+    variables(
+      :is_root => user == 'root'
+    )
+    mode 0640
+    owner user
+    group user
+  end
+
+  template "#{home}/.vimrc" do
+    source "vimrc.erb"
+    mode 0640
+    owner user
+    group user
+  end
+
+  template "#{home}/.inputrc" do
+    source "inputrc.erb"
+    mode 0640
+    owner user
+    group user
+  end
+
+  template "#{home}/.bashrc" do
+    source "bashrc.erb"
+    mode 0640
+    owner user
+    group user
+  end
+
+  template "#{home}/.bashrc.d/05-settings" do
+    source "bashrc.d/05-settings.erb"
+    mode 0740
+    owner user
+    group user
+  end
+
+  template "#{home}/.bashrc.d/10-path" do
+    source "bashrc.d/10-path.erb"
+    mode 0740
+    owner user
+    group user
+  end
+
+  template "#{home}/.bashrc.d/20-functions" do
+    source "bashrc.d/20-functions.erb"
+    mode 0740
+    owner user
+    group user
+  end
+
+  template "#{home}/.bashrc.d/30-aliases" do
+    source "bashrc.d/30-aliases.erb"
+    mode 0740
+    owner user
+    group user
+  end
+
+  template "#{home}/.bashrc.d/40-prompt" do
+    source "bashrc.d/40-prompt.erb"
+    mode 0740
+    owner user
+    group user
+  end
+
+  template "#{home}/.bashrc.d/50-other" do
+    source "bashrc.d/50-other.erb"
+    mode 0740
+    owner user
+    group user
+  end
+end
+
 package "build-essential"
+package "ruby"
 
 package "vim-gtk"
 package "screen"
@@ -145,3 +215,7 @@ package "curl"
 
 package "logrotate"
 package "checkinstall"
+
+package "dosbox"
+package "wine"
+package "wine-gecko"

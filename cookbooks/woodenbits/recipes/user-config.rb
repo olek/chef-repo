dc2d3237a58097a3ea0675ced398516458eea0cf
Olek Poplavsky <olek@woodenbits.com>
2013-06-02 19:21:30 -0400

    updated git config setup

--- cookbooks/woodenbits/recipes/user-config.rb
+++ cookbooks/woodenbits/recipes/user-config.rb
@@ -3,6 +3,53 @@
 
 users = ['olek']
 
+git_configs = {
+  "alias.br" => "branch",
+  "alias.bra" => "branch -a",
+  "alias.ci" => "commit",
+  "alias.co" => "checkout",
+  "alias.cp" => "cherry-pick",
+  "alias.dc" => "diff --cached",
+  "alias.di" => "diff",
+  "alias.g" => "log --pretty=format:\"%h %an - %s\" --graph",
+  "alias.gg" => "log --pretty=format:\"%H %an - %s\" --graph",
+  "alias.lc" => "log ORIG_HEAD.. --stat --no-merges",
+  "alias.ll" => "log --pretty=format:\"%Cred%h %Cblue%an %Cgreen%s / %Cblue%ar%Creset\" --abbrev-commit -n15",
+  "alias.st" => "status",
+  "alias.s" => "status --short",
+  "alias.w" => "whatchanged",
+  "alias.pull-ff" => "pull --ff-only",
+  "alias.edit-unmerged" => "!f() { git ls-files --unmerged | cut -f2 | sort -u ; }; tvim `f`",
+  "alias.add-unmerged" => "!f() { git ls-files --unmerged | cut -f2 | sort -u ; }; git add `f`",
+#  "alias.down" => "!sh -c \"CURRENT=$(git symbolic-ref HEAD | sed -e s@.*/@@) && (git pull --ff-only || (git fetch origin && git rebase --preserve-merges origin/$CURRENT))\"",
+#  "alias.publish" => "!f() { if [ $# -ne 1 ]; then echo \"usage: git publish <local-branch-name>\" >&2; exit 1; fi; git push --set-upstream origin $1:$1; }; f",
+#  "alias.unpublish" => "!f() { if [ $# -ne 1 ]; then echo \"usage: git unpublish <remote-branch-name>\" >&2; exit 1; fi; git push origin :$1; }; f",
+
+  "color.branch" => "auto",
+  "color.diff" => "auto",
+  "color.interactive" => "auto",
+  "color.status" => "auto",
+  "color.ui" => "true",
+  "color.grep" => "auto",
+
+  "core.excludesfile" => "~/.gitignore",
+
+  "branch.autosetuprebase" => "never",
+  "branch.autosetupmerge" => "true",
+
+  "apply.whitespace" => "fix",
+  "grep.lineNumber " => "true",
+  "pull.rebase" => "true",
+  "push.default" => "current",
+
+  "diff.tool" => "diffmerge",
+  "difftool.diffmerge.cmd" => "diffmerge \"$LOCAL\" \"$REMOTE\"",
+  "merge.tool" => "diffmerge",
+  "mergetool.diffmerge.cmd" => "diffmerge --merge --result=\"$MERGED\" \"$LOCAL\" \"$(if test -f \"$BASE\"; then echo \"$BASE\"; else echo \"$LOCAL\"; fi)\" \"$REMOTE\"",
+  "mergetool.diffmerge.trustExitCode" => "true",
+}
+
+
 template '/etc/sudoers.d/truecrypt' do
   source 'system/etc/sudoers-truecrypt.erb'
   variables(
@@ -45,6 +92,7 @@ end
     group user_group
   end
 
+=begin
   template "#{home_dir}/.gitconfig" do
     source 'gitconfig.erb'
     variables(
@@ -56,6 +104,7 @@ end
     owner user
     group user_group
   end
+=end
 
   template "#{home_dir}/.screenrc" do
     source 'screenrc.erb'
@@ -143,14 +192,30 @@ if user && user != 'root'
     command %Q(#{sudo} "gsettings set com.canonical.Unity2d use-opengl true")
     only_if %Q(#{sudo} "gsettings get com.canonical.Unity2d use-opengl | grep false")
   end
+
+  template "#{home_dir}/chef-notes.txt" do
+    source 'chef-notes.erb'
+    mode '0600'
+    owner user
+    group user_group
+  end
+
+  local_git_configs = git_configs
+
+  if user == 'olek'
+    local_git_configs = git_configs.merge(
+      'user.name' => 'Olek Poplavsky',
+      'user.email' => 'olek@woodenbits.com'
+    )
+  end
+
+  local_git_configs.each do |k, v|
+    execute "set global git config #{k}" do
+      command "git config --global #{k} \'#{v}\'"
+      user user
+      not_if "git config --global #{k} | grep -q \'#{v}\'"
+    end
+  end
 else
-  Chef::Log.info "No gnome settings changes performed for user #{user}"
+  Chef::Log.info "No gnome/git settings changes performed for user #{user.inspect}"
 end
-
-template "#{home_dir}/chef-notes.txt" do
-  source 'chef-notes.erb'
-  mode '0600'
-  owner user
-  group user_group
-end
-

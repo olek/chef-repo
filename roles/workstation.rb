2379754e252c37c7dfda96706191f98c5146fec2
Olek Poplavsky <olek@woodenbits.com>
2013-12-29 21:25:22 -0500

    good bye rvm, hello again rbenv

--- roles/workstation.rb
+++ roles/workstation.rb
@@ -1,4 +1,4 @@
 name 'workstation'
 description 'Wraps all required recipes for my typical workstation setup.'
 
-recipes %w(sys-repos sys-config sys-packages truecrypt user-init user-config rvm modcloth).map { |name| "woodenbits::#{name}" }
+recipes %w(sys-repos sys-config sys-packages truecrypt user-init user-config rbenv modcloth).map { |name| "woodenbits::#{name}" }

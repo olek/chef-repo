960a683fe04385ec901ba6ae110fae8a3706dc69
Olek Poplavsky <olek@woodenbits.com>
2016-03-13 16:13:40 -0400

    added metadata to woodenbits cookbook to make chef v12 happy

---
+++ cookbooks/woodenbits/metadata.rb
@@ -0,0 +1,7 @@
+name             'woodenbits'
+maintainer       'Woodenbits'
+maintainer_email 'polek@gmx.net'
+license          'All rights reserved'
+description      'Installs/Configures woodenbits'
+long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
+version          '0.0.0'
